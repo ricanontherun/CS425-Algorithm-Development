148105321ce3e7e567a96899e2edd1809363d2af
christian <ricanontherun@gmaill.com>
2017-03-25 21:17:49 -0400

    btree

--- week4/binary_search_tree.cpp
+++ week4/binary_search_tree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <utility>
+#include <cassert>
 
 template <typename T>
 class BinarySearchTree
@@ -8,17 +10,39 @@ class BinarySearchTree
 			: root(nullptr)
 		{};
 
-		bool insert(T object) {
-			if ( this->root == nullptr ) {
-				std::cout << "Setting new root\n";
-				this->root = new Node(object);
-			} else {
-				return this->insert(object, this->root);
-			}
-
-			return true;
+		~BinarySearchTree()
+		{
+			this->clear();
 		}
 
+		bool insert(const T & object) {
+			return this->insert(object, this->root);
+		}
+
+		bool contains(const T & object) const {
+			return this->contains(object, this->root);
+		}
+
+		void remove(const T & object)
+		{
+			this->remove(object, this->root);
+		}
+
+		// In order traversal.
+		void print()
+		{
+			this->print(this->root);
+		}
+
+		void clear()
+		{
+			this->emptyTree(this->root);
+		}
+
+		bool empty() const
+		{
+			return this->root == nullptr;
+		}
 	private:
 		struct Node
 		{
@@ -28,29 +52,96 @@ class BinarySearchTree
 
 			Node(T data, Node * left = nullptr, Node * right = nullptr)
 				: data(data),
-					left(left),
-					right(right)
+				left(left),
+				right(right)
 			{}
 		};
 
-		bool insert(T object, Node * root) {
+		// * & - Pointer to a reference? Wtf
+		bool insert(T object, Node * & root) {
 			bool ret = true;
 
 			if ( root == nullptr ) {
-				std::cout << "Setting new root\n";
 				root = new Node(object);
 			} else if ( object < root->data ) {
-				std::cout << "Going left\n";
 				this->insert(object, root->left);
 			} else if ( object > root->data ) {
-				std::cout << "Going right\n";
 				this->insert(object, root->right);
-			} else {
-				std::cout << "Found a duplicate\n";
+			} else { // Duplicate, an error in this implementation.
 				ret = false;
 			}
 
 			return ret;
+		}
+
+		bool contains(const T & object, const Node * root) const
+		{
+			if ( root == nullptr ) {
+				return false;
+			} else if ( object < root->data  ) {
+				return this->contains(object, root->left);
+			} else if ( object > root->data ) {
+				return this->contains(object, root->right);
+			} else {
+				return true; // Match
+			}
+		}
+
+		void remove(const T & object, Node * & root)
+		{
+			// We've reached a a leaf without finding the item.
+			if ( root == nullptr ) {
+				return;
+			}
+
+			if ( object < root->data  ) {
+				this->remove(object, root->left);
+			} else if ( object > root->data ) {
+				this->remove(object, root->right);
+			} else if ( root->left != nullptr && root->right != nullptr ) { // Two children.
+				root->data = this->findMin(root->right)->data;
+				this->remove(root->data, root->right);
+			} else { // One child.
+				Node * old = root;
+				root = ( root->left != nullptr ) ? root->left : root->right;
+				delete old;
+			}
+		}
+
+		void print(const Node * node) const
+		{
+			if ( node == nullptr ) {
+				return;
+			}
+
+			this->print(node->left);
+			std::cout << node->data << "\n";
+			this->print(node->right);
+		}
+
+		void emptyTree(Node * & root)
+		{
+			if ( root != nullptr ) { // Base case
+				this->emptyTree(root->left);
+				this->emptyTree(root->right);
+				delete root;
+			}
+
+			root = nullptr;
+		}
+
+		Node * findMin(Node * root) const
+		{
+			if ( root == nullptr ) {
+				return nullptr;
+			}
+
+			// No smaller child, this is the smallest child.
+			if ( root->left == nullptr ) {
+				return root;
+			}
+
+			return this->findMin(root->left);
 		}
 
 		Node * root;
@@ -59,6 +150,34 @@ class BinarySearchTree
 int main() {
 	BinarySearchTree<int> tree;
 
+	tree.insert(2);
+	tree.insert(7);
 	tree.insert(1);
 	tree.insert(3);
+	tree.insert(10);
+
+	assert(!tree.empty());
+
+	assert(tree.contains(7));
+	assert(tree.contains(1));
+	assert(tree.contains(3));
+	assert(tree.contains(2));
+
+	assert(!tree.contains(30));
+
+	tree.clear();
+	assert(tree.empty());
+
+	tree.insert(2);
+	tree.insert(7);
+	tree.insert(1);
+	tree.insert(3);
+	tree.insert(10);
+
+	assert(!tree.empty());
+
+	tree.remove(7);
+	assert(!tree.contains(7));
+
+	tree.print();
 }
